22a2ea0301c499baf84f9351d33614dcfed16782
Qishen Wu <22854142+wuqs-net@users.noreply.github.com>
2022-11-19 23:22:17 -0500

    Add `api/` in API base URL

--- api/api/urls.py
+++ api/api/urls.py
@@ -29,7 +29,7 @@ router.register(r'textbox', views.TutorialTextBoxViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

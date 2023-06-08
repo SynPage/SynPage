4e22b38779b86e59d2367045ba346a071bcc72ce
Peter Yang <peter@reviewerly.io>
2023-06-08 19:15:04 -0400

    update chrome extension favicon and description

--- browser-extension/src/Popup/components/SearchBar.tsx
+++ browser-extension/src/Popup/components/SearchBar.tsx
@@ -1,14 +1,16 @@
-import {IconButton, InputAdornment, TextField} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
-import {useState} from "react";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
+import "./SearchBar.css"; // Import the CSS file for styling
 
 export const SearchBar = (props: { onSearchFor: (query: string) => void }) => {
-  const {onSearchFor, ...others} = props;
+  const { onSearchFor, ...others } = props;
   const [text, setText] = useState("");
 
   return (
     <form
+      className="search-bar" // Add a CSS class to the form element
       onSubmit={(e) => {
         e.preventDefault();
         onSearchFor(text);
@@ -33,20 +35,19 @@ export const SearchBar = (props: { onSearchFor: (query: string) => void }) => {
           startAdornment: (
             <InputAdornment position={"start"}>
               <IconButton type="submit" aria-label="search">
-                <SearchIcon/>
+                <SearchIcon />
               </IconButton>
             </InputAdornment>
           ),
           endAdornment: (
             <InputAdornment position={"end"}>
               <IconButton type="reset" aria-label="close">
-                <CloseIcon/>
+                <CloseIcon />
               </IconButton>
             </InputAdornment>
-          )
+          ),
         }}
       />
-
     </form>
-  )
+  );
 };

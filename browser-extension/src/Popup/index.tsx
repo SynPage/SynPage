c61cbf0d8d73d31b2384150b6f1c54ca3fcd1876
ypatel2022 <yaxpatel2004@gmail.com>
2023-06-09 15:44:50 -0400

    change styles for alert messaage

--- browser-extension/src/Popup/index.tsx
+++ browser-extension/src/Popup/index.tsx
@@ -1,229 +1,272 @@
-import React, {createContext, useEffect, useState} from 'react';
-import {Header} from "./components/Header";
-import {TutorialList} from "./components/TutorialList";
-import {Loading} from "../shared/Loading";
-import {Error} from "../shared/Error";
-import {ActionsApi, StepsApi, Tutorial, TutorialInfo, TutorialsApi} from "../client/generated";
-import {QueryType} from "../chrome/query";
-import {Status, validateResponse} from "../chrome/response";
-import {PopupClient} from "../chrome/popupClient";
-import "./index.css"
+import React, { createContext, useEffect, useState } from 'react'
+import { Header } from './components/Header'
+import { TutorialList } from './components/TutorialList'
+import { Loading } from '../shared/Loading'
+import { Error } from '../shared/Error'
 import {
-	Alert,
-	BottomNavigation,
-	BottomNavigationAction,
-	Box, Button,
-	Card,
-	CardActionArea, Dialog, DialogActions, DialogTitle,
-	Grid, IconButton,
-	InputBase,
-	Paper,
-	createTheme,
-	ThemeProvider
-} from "@mui/material";
-import HomeIcon from '@mui/icons-material/Home';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
-import {TutorialGenerationComponent} from "./components/TutorialGeneration";
-import SearchIcon from "@mui/icons-material/Search";
-
+  ActionsApi,
+  StepsApi,
+  Tutorial,
+  TutorialInfo,
+  TutorialsApi,
+} from '../client/generated'
+import { QueryType } from '../chrome/query'
+import { Status, validateResponse } from '../chrome/response'
+import { PopupClient } from '../chrome/popupClient'
+import './index.css'
+import {
+  Alert,
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+  createTheme,
+  ThemeProvider,
+  Container,
+} from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import SmartToyIcon from '@mui/icons-material/SmartToy'
+import { TutorialGenerationComponent } from './components/TutorialGeneration'
+import SearchIcon from '@mui/icons-material/Search'
 
 export interface PopupProps {
-	chromeClient: PopupClient,
-	tutorialsApi: TutorialsApi,
-	stepsApi: StepsApi,
-	actionsApi: ActionsApi
+  chromeClient: PopupClient
+  tutorialsApi: TutorialsApi
+  stepsApi: StepsApi
+  actionsApi: ActionsApi
 }
 
 interface Context {
-	chromeClient: PopupClient
-	tutorialsApi: TutorialsApi
-	stepsApi: StepsApi
-	actionsApi: ActionsApi
+  chromeClient: PopupClient
+  tutorialsApi: TutorialsApi
+  stepsApi: StepsApi
+  actionsApi: ActionsApi
 }
 
-export const ClientContext = createContext<Context>(
-	{
-		chromeClient: new PopupClient(),
-		tutorialsApi: new TutorialsApi(),
-		stepsApi: new StepsApi(),
-		actionsApi: new ActionsApi()
-	}
-);
+export const ClientContext = createContext<Context>({
+  chromeClient: new PopupClient(),
+  tutorialsApi: new TutorialsApi(),
+  stepsApi: new StepsApi(),
+  actionsApi: new ActionsApi(),
+})
 
 export enum Tab {
-	TUTORIALS,
-	AI,
+  TUTORIALS,
+  AI,
 }
 
 const theme = createTheme({
-	typography: {
-	  fontFamily: [
-		'Inter',
-		'sans-serif',
-	  ].join(','),
-	},
-  });
-  
-
+  typography: {
+    fontFamily: ['Inter', 'sans-serif'].join(','),
+  },
+})
 
 export const Popup = (props: PopupProps) => {
-	const {chromeClient, tutorialsApi, stepsApi, actionsApi} = props;
-	const [list, setList] = useState<TutorialInfo[]>([]);
-	const [loading, setLoading] = useState<string | undefined>(undefined);
-	const [error, setError] = useState<string | undefined>(undefined);
-	const [tab, setTab] = useState<Tab>(Tab.TUTORIALS);
-	const [ready, setReady] = useState<boolean>(false);
+  const { chromeClient, tutorialsApi, stepsApi, actionsApi } = props
+  const [list, setList] = useState<TutorialInfo[]>([])
+  const [loading, setLoading] = useState<string | undefined>(undefined)
+  const [error, setError] = useState<string | undefined>(undefined)
+  const [tab, setTab] = useState<Tab>(Tab.TUTORIALS)
+  const [ready, setReady] = useState<boolean>(false)
 
-	useEffect(() => {
-		// chromeClient.listen(handleChromeMessage, handleChromeError);
-		console.log(chrome);
+  useEffect(() => {
+    // chromeClient.listen(handleChromeMessage, handleChromeError);
+    console.log(chrome)
 
-		setLoading("Loading...");
-		tutorialsApi.tutorialsList().then(value => {
-			setLoading(undefined);
-			if (!value.results) {
-				setError("Failed to get tutorials for site.");
-			} else {
-				setList(value.results);
-			}
-		}, reason => {
-			setLoading(undefined)
-			setError(reason.message ?? "Unknown error.");
-		});
-	}, []);
+    setLoading('Loading...')
+    tutorialsApi.tutorialsList().then(
+      (value) => {
+        setLoading(undefined)
+        if (!value.results) {
+          setError('Failed to get tutorials for site.')
+        } else {
+          setList(value.results)
+        }
+      },
+      (reason) => {
+        setLoading(undefined)
+        setError(reason.message ?? 'Unknown error.')
+      }
+    )
+  }, [])
 
-	// const handleChromeMessage = async (query: ChromeQuery): Promise<ChromeResponse> => {
-	//   console.log("[Popup]: Received query", query)
-	//   switch (query.type) {
-	//     default:
-	//       const error = "[Popup]: Unexpected query type."
-	//       setError(error);
-	//       return {
-	//         query: query,
-	//         status: Status.error,
-	//         message: error
-	//       }
-	//   }
-	// }
+  // const handleChromeMessage = async (query: ChromeQuery): Promise<ChromeResponse> => {
+  //   console.log("[Popup]: Received query", query)
+  //   switch (query.type) {
+  //     default:
+  //       const error = "[Popup]: Unexpected query type."
+  //       setError(error);
+  //       return {
+  //         query: query,
+  //         status: Status.error,
+  //         message: error
+  //       }
+  //   }
+  // }
 
-	// const handleChromeError = async (query: ChromeQuery, message: string): Promise<ChromeResponse> => {
-	//   setError(message);
-	//   return {
-	//     query: query,
-	//     status: Status.error,
-	//     message: message
-	//   }
-	// }
+  // const handleChromeError = async (query: ChromeQuery, message: string): Promise<ChromeResponse> => {
+  //   setError(message);
+  //   return {
+  //     query: query,
+  //     status: Status.error,
+  //     message: message
+  //   }
+  // }
 
-	const handleTutorialSelection = (tutorial: TutorialInfo) => {
-		if (!tutorial.id) {
-			setError("Tutorial does not exist.");
-			return;
-		}
-		setLoading("Loading...");
-		console.log(`[Popup]: Notifying background of tutorial selection`, tutorial);
-		chromeClient.queryBackground({type: QueryType.init, message: tutorial.id}).then(chromeResponse => {
-			setLoading(undefined)
-			const {valid, validated} = validateResponse(chromeResponse);
-			if (!valid) {
-				console.log("Received unexpected response.", chromeResponse);
-				setError("Received unexpected response.");
-			} else if (chromeResponse.status !== Status.ok) {
-				setError(validated.message ?? "Unknown error.");
-			} else {
-				setReady(true);
-			}
-		}, reason => {
-			console.log("[Popup]: Error occurred when trying to reach background.");
-			setLoading(undefined);
-			setError(reason.message ?? "Unknown error.");
-		})
+  const handleTutorialSelection = (tutorial: TutorialInfo) => {
+    if (!tutorial.id) {
+      setError('Tutorial does not exist.')
+      return
+    }
+    setLoading('Loading...')
+    console.log(`[Popup]: Notifying background of tutorial selection`, tutorial)
+    chromeClient
+      .queryBackground({ type: QueryType.init, message: tutorial.id })
+      .then(
+        (chromeResponse) => {
+          setLoading(undefined)
+          const { valid, validated } = validateResponse(chromeResponse)
+          if (!valid) {
+            console.log('Received unexpected response.', chromeResponse)
+            setError('Received unexpected response.')
+          } else if (chromeResponse.status !== Status.ok) {
+            setError(validated.message ?? 'Unknown error.')
+          } else {
+            setReady(true)
+          }
+        },
+        (reason) => {
+          console.log(
+            '[Popup]: Error occurred when trying to reach background.'
+          )
+          setLoading(undefined)
+          setError(reason.message ?? 'Unknown error.')
+        }
+      )
 
-		// getCurrentTab().then(tab => {
-		//   chromeClient.queryBackground({type: QueryType.accessibility, message: {tabId: tab.id, locator: ""}}).then(chromeResponse => {
-		//     console.log("AOM", chromeResponse);
-		//     const nodeId: number = chromeResponse.message;
-		//   }, reason => {
-		//     console.log("[Popup]: Error occurred when trying to reach background.");
-		//     setLoading(undefined);
-		//     setError(reason.message ?? "Unknown error.");
-		//   })
-		// })
-	}
+    // getCurrentTab().then(tab => {
+    //   chromeClient.queryBackground({type: QueryType.accessibility, message: {tabId: tab.id, locator: ""}}).then(chromeResponse => {
+    //     console.log("AOM", chromeResponse);
+    //     const nodeId: number = chromeResponse.message;
+    //   }, reason => {
+    //     console.log("[Popup]: Error occurred when trying to reach background.");
+    //     setLoading(undefined);
+    //     setError(reason.message ?? "Unknown error.");
+    //   })
+    // })
+  }
 
-	const handleSearch = (question: string) => {
-		setLoading("Loading Tutorials...");
-		tutorialsApi.tutorialsList({search: question}).then(value => {
-			setLoading(undefined);
-			setList(value.results ?? []);
-		});
-	}
+  const handleSearch = (question: string) => {
+    setLoading('Loading Tutorials...')
+    tutorialsApi.tutorialsList({ search: question }).then((value) => {
+      setLoading(undefined)
+      setList(value.results ?? [])
+    })
+  }
 
-	return (
-		<ThemeProvider theme={theme}>
-			<ClientContext.Provider value={{
-				chromeClient: chromeClient,
-				tutorialsApi: tutorialsApi,
-				stepsApi: stepsApi,
-				actionsApi: actionsApi
-			}}>
-				<Header/>
-				{/*<Paper sx={{position: 'static', top: 0, left: 0, right: 0}} elevation={5}>*/}
-				{/*	*/}
-				{/*</Paper>*/}
-				<Box paddingY={8}>
-					<Loading loading={loading}/>
-					<Error error={error}/>
-					{!loading && tab === Tab.TUTORIALS &&
-						<>
-							<Paper
-								component="form"
-								sx={{p: '2px 4px', display: 'flex', alignItems: 'center', marginBottom: 3}}
-								elevation={3}
-								onSubmit={(event) => {
-									event.preventDefault();
-									handleSearch(event.currentTarget.querySelector("input")?.value ?? "");
-								}}
-							>
-								<InputBase
-									sx={{ml: 1, flex: 1}}
-									placeholder="What do you want to learn?"
-									inputProps={{'aria-label': 'search tutorials'}}
-								/>
-								<IconButton type="submit" sx={{p: '10px'}} aria-label="search">
-									<SearchIcon/>
-								</IconButton>
-							</Paper>
-							<TutorialList tutorials={list} onTutorialSelection={handleTutorialSelection}/>
-							<Card>
-								<CardActionArea onClick={() => setTab(Tab.AI)}>
-									<Alert severity="info">
-										Cannot find what you are looking for? Try asking the AI {">"}
-									</Alert>
-								</CardActionArea>
-							</Card>
-						</>}
-					{!loading && tab === Tab.AI && <TutorialGenerationComponent onTutorialSelection={handleTutorialSelection}/>}
-				</Box>
-				<Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 10}} elevation={5}>
-					<BottomNavigation
-						showLabels
-						value={tab}
-						onChange={(event, newValue) => {
-							setTab(newValue);
-						}}
-					>
-						<BottomNavigationAction icon={<HomeIcon/>}/>
-						<BottomNavigationAction icon={<SmartToyIcon/>}/>
-					</BottomNavigation>
-				</Paper>
-				<Dialog open={ready} onClose={() => setReady(false)}>
-					<DialogTitle>Tutorial is ready!</DialogTitle>
-					<DialogActions>
-						<Button sx={{ textTransform: 'none' }} onClick={() => window.close()}>Start your tour now!</Button>
-					</DialogActions>
-				</Dialog>
-			</ClientContext.Provider>
-		</ThemeProvider>
-	);
-};
+  return (
+    <ThemeProvider theme={theme}>
+      <ClientContext.Provider
+        value={{
+          chromeClient: chromeClient,
+          tutorialsApi: tutorialsApi,
+          stepsApi: stepsApi,
+          actionsApi: actionsApi,
+        }}
+      >
+        <Header />
+        {/*<Paper sx={{position: 'static', top: 0, left: 0, right: 0}} elevation={5}>*/}
+        {/*	*/}
+        {/*</Paper>*/}
+        <Box paddingY={8}>
+          <Loading loading={loading} />
+          <Error error={error} />
+          {!loading && tab === Tab.TUTORIALS && (
+            <>
+              <Paper
+                component='form'
+                sx={{
+                  p: '2px 4px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginBottom: 3,
+                }}
+                elevation={3}
+                onSubmit={(event) => {
+                  event.preventDefault()
+                  handleSearch(
+                    event.currentTarget.querySelector('input')?.value ?? ''
+                  )
+                }}
+              >
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder='What do you want to learn?'
+                  inputProps={{ 'aria-label': 'search tutorials' }}
+                />
+                <IconButton
+                  type='submit'
+                  sx={{ p: '10px' }}
+                  aria-label='search'
+                >
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+              <TutorialList
+                tutorials={list}
+                onTutorialSelection={handleTutorialSelection}
+              />
+              <Card sx={{ mx: 2, borderRadius: 2 }}>
+                <CardActionArea onClick={() => setTab(Tab.AI)}>
+                  <Alert severity='info'>
+                    Can't find what you're looking for? Try asking the AI {'>'}
+                  </Alert>
+                </CardActionArea>
+              </Card>
+            </>
+          )}
+          {!loading && tab === Tab.AI && (
+            <TutorialGenerationComponent
+              onTutorialSelection={handleTutorialSelection}
+            />
+          )}
+        </Box>
+        <Paper
+          sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 10 }}
+          elevation={5}
+        >
+          <BottomNavigation
+            showLabels
+            value={tab}
+            onChange={(event, newValue) => {
+              setTab(newValue)
+            }}
+          >
+            <BottomNavigationAction icon={<HomeIcon />} />
+            <BottomNavigationAction icon={<SmartToyIcon />} />
+          </BottomNavigation>
+        </Paper>
+        <Dialog open={ready} onClose={() => setReady(false)}>
+          <DialogTitle>Tutorial is ready!</DialogTitle>
+          <DialogActions>
+            <Button
+              sx={{ textTransform: 'none' }}
+              onClick={() => window.close()}
+            >
+              Start your tour now!
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </ClientContext.Provider>
+    </ThemeProvider>
+  )
+}

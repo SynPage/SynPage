17f92fbe83b9c9ce8acb45082eff7736b58190d0
ypatel2022 <yaxpatel2004@gmail.com>
2023-06-10 20:15:35 -0400

    reduce build times by 7 seconds

--- browser-extension/webpack.config.js
+++ browser-extension/webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require('dotenv-webpack');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: {
     index: './src/index.tsx',
     background: './src/background.ts',
-    content: './src/content.tsx'
+    content: './src/content.tsx',
   },
   output: {
     publicPath: '',
-    path: path.join(__dirname, "build"),
+    path: path.join(__dirname, 'build'),
     // filename: '[name].[contenthash].bundle.js',
     filename: '[name].js',
-    clean: true
+    clean: true,
   },
   // mode: process.env.NODE_ENV || "development",
-  mode: "development",
+  mode: 'development',
   devtool: 'cheap-module-source-map',
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
-  devServer: { contentBase: path.join(__dirname, "src") },
+  devServer: { contentBase: path.join(__dirname, 'src') },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        // Match js, jsx, ts & tsx files
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
         exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
+        options: {
+          // JavaScript version to compile to
+          target: 'es2015',
+        },
       },
       {
         test: /\.(css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/,
@@ -63,22 +63,22 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-      chunks: ["index"]
+      template: path.join(__dirname, 'public', 'index.html'),
+      chunks: ['index'],
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: "public",
+          from: 'public',
           globOptions: {
-            ignore: ["**/index.html"],
-          }
+            ignore: ['**/index.html'],
+          },
         },
       ],
     }),
     new MiniCssExtractPlugin(),
     new Dotenv({
-      path: ".env"
-    })
+      path: '.env',
+    }),
   ],
-};
+}
